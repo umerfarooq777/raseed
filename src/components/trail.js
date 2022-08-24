cd3c885c6cc49215731e42d77d5fc88da1a33297
Umer Farooq <umerfarooq03100276796@gmail.com>
2022-08-24 23:01:45 +0500

    trail formmated

--- src/components/trail.js
+++ src/components/trail.js
@@ -8,163 +8,157 @@ import Loader from "./loader";
 import { TrailBalanceContext } from "../context/trailBalanceContext";
 // import TableContent from "react-bootstrap/TableContent";
 
-function Ledger() {
+function TrailBalanceSheet() {
   const [generalRecords, setGeneralRecords] = useState([]);
-  const [firebaseData,setFirebaseData] =useState([]);
+  const [firebaseData, setFirebaseData] = useState([]);
   // console.log(firebaseData)
 
-  const [debitBal, setDebitBal] = useState(0)
-  
-  // console.log("🚀 ~ file: trail.js ~ line 17 ~ Ledger ~ debitBal", debitBal)
-  const [creditBal, setCreditBal] = useState(0)
 
-  
-  const [balance, setBalance] = useState(0)
+  const [balances, setBalances] = useState([]);
+  // console.log("🚀 ~ file: trail.js ~ line 18 ~ TrailBalanceSheet ~ balances", balances)
+
+
+  const [debitBal, setDebitBal] = useState(0);
+
+  // console.log("🚀 ~ file: trail.js ~ line 17 ~ Ledger ~ debitBal", debitBal)
+  const [creditBal, setCreditBal] = useState(0);
+
+  const [balance, setBalance] = useState(0);
   // console.log("🚀 ~ file: trail.js ~ line 22 ~ Ledger ~ balance", balance)
-  const [titles, setTitles] = useState()
+  const [titles, setTitles] = useState();
   // console.log("🚀 ~ file: ledger.js ~ line 15 ~ Ledger ~ titles", titles)
   const dbRef = ref(FirebaseStack());
-////=========================================================
+  ////=========================================================
 
-useEffect(() => {
-  getDataFromFirebase();
-}, []);
+  useEffect(() => {
+    getDataFromFirebase();
+  }, []);
 
   const getDataFromFirebase = async () => {
     get(child(dbRef, `transactions/`))
       .then((snapshot) => {
         if (snapshot.exists()) {
-        //   setFirebaseData(snapshot.val());
-        //   setStopData(false);
-        // setGeneralRecords(snapshot.val())
-        setFirebaseData(snapshot.val())
-        
-        // console.log(firebaseData) 
+          //   setFirebaseData(snapshot.val());
+          //   setStopData(false);
+          // setGeneralRecords(snapshot.val())
+          setFirebaseData(snapshot.val());
+
+          // console.log(firebaseData)
         } else {
           console.log("No data available");
         }
       })
       .catch((error) => {
-        console.error(error); 
+        console.error(error);
       });
   };
 
   useEffect(() => {
     getAccountsFromFirebase();
   }, []);
-  
-    const getAccountsFromFirebase = async () => {
-      get(child(dbRef, `accounts/`))
-        .then((snapshot) => {
-          if (snapshot.exists()) {
+
+  const getAccountsFromFirebase = async () => {
+    get(child(dbRef, `accounts/`))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
           //   setFirebaseData(snapshot.val());
           //   setStopData(false);
           // setGeneralRecords(snapshot.val())
-          setTitles(snapshot.val())
-          
-          // console.log(firebaseData) 
-          } else {
-            console.log("No data available");
-          }
-        })
-        .catch((error) => {
-          console.error(error); 
-        });
-    };
+          setTitles(snapshot.val());
 
-    useEffect(()=>{
-            getTrailBalance()
-        },[titles])
-    
-        const getTrailBalance = () => {
-            // var balance = 0;
-            firebaseData && firebaseData.map((obj) => {
-                if (obj.data.type === 'debit') {
-                  setBalance(balance+(Number(obj.data.amount)))
-                }
-                else if(obj.data.type === 'credit'){
-                  setBalance(balance-(Number(obj.data.amount)))
-                }
-            })
-            setBalance(balance)
+          // console.log(firebaseData)
+        } else {
+          console.log("No data available");
         }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
+  useEffect(() => {
+    getTrailBalance();
+  }, [titles]);
 
-        
-
-
+  const getTrailBalance = () => {
+    // var balance = 0;
+    firebaseData &&
+      firebaseData.map((obj) => {
+        // console.log("🚀 ~ file: trail.js ~ line 82 ~ firebaseData&&firebaseData.map ~ obj", obj,)
+        if (obj.data.type === "debit") {
+          setBalance(balance + Number(obj.data.amount));
+        } else if (obj.data.type === "credit") {
+          setBalance(balance - Number(obj.data.amount));
+        }
+      });
+    setBalance(balance);
+  };
 
   ////=========================================================
   return (
     <>
-      <Container><TrailBalanceContext.Provider value={{debitBal,setDebitBal,creditBal,setCreditBal}}>
+      <Container>
+        <TrailBalanceContext.Provider
+          value={{ debitBal, setDebitBal, creditBal, setCreditBal,balances,setBalances }}
+        >
+          <Table
+            striped
+            bordered
+            hover
+            variant="dark"
+            className="table-card mt-4"
+          >
+            <thead className="account-card">
+              <tr>
+                <th colSpan={7}>
+                  <h4 className="ledger-title capitalize text-center ">
+                    Trail Balance Sheet
+                  </h4>
+                </th>
+              </tr>
 
-                   <Table striped bordered hover variant="dark" className="table-card mt-4">
-                        <thead className="account-card">
-                        <tr>
-                          <th colSpan={7}>
-                            <h4 className="ledger-title capitalize text-center ">
-                              Trail Balance Sheet
-                            </h4>
-                          </th>
-                        </tr>
-
-
-                        <tr>
-                          <th>Date</th>
-                          <th colSpan={4} >Description/Account</th>
-                          <th>Debit</th>
-                          <th>Credit</th>
-                        </tr>
-                      </thead>          
-         
+              <tr>
+                <th>Date</th>
+                <th colSpan={4}>Description/Account</th>
+                <th>Debit</th>
+                <th>Credit</th>
+              </tr>
+            </thead>
 
             {titles ? (
               titles.map((obj, key) => {
-                
-                let account = Object.assign({},titles)
-                
-                
-                
+                let account = Object.assign({}, titles);
+
                 return (
-                  <> 
-                  
-                  
-                    <RecordLedger array={firebaseData} keys={key} accounts={account} bal={balance} />
-                  
-         
-                   
+                  <>
+                    <RecordLedger
+                      array={firebaseData}
+                      keys={key}
+                      accounts={account}
+                      bal={balance}
+                    />
                   </>
                 );
-
               })
             ) : (
-            <>
-            <Loader/>
+              <>
+                <Loader />
               </>
             )}
 
-                  <tbody>
-                    <tr>
-                      <td></td>
-                      <td colSpan={4} className="capitalize"></td> 
-                      <td className="balanace">
-                      {
-                          debitBal
-                        }
-                      </td> 
-                      <td className="balanace">
-                        {
-                          creditBal
-                        }
-                      </td>
-                    </tr>
-                  </tbody>  
-            </Table>
-            </TrailBalanceContext.Provider>
+            <tbody>
+              <tr>
+                <td></td>
+                <td colSpan={4} className="capitalize"></td>
+                <td className="balanace">{debitBal}</td>
+                <td className="balanace">{creditBal}</td>
+              </tr>
+            </tbody>
+          </Table>
+        </TrailBalanceContext.Provider>
       </Container>
     </>
   );
 }
 
-export default Ledger;
+export default TrailBalanceSheet;
--- src/components/trailRecord.js
+++ src/components/trailRecord.js
@@ -1,12 +1,21 @@
-import React, { useEffect, useState,useContext } from "react";
+import React, { useEffect, useState, useContext } from "react";
 // import TableContentSub from './TableContentSub'
 
 import FirebaseStack from "../firebase-config";
 import { ref, get, child, set } from "firebase/database";
 import { TrailBalanceContext } from "../context/trailBalanceContext";
+import TrailSet from "./trailSet";
 
-const RecordLedger = ({ array, keys, accounts, bal }) => {
-// console.log("🚀 ~ file: recordLedger.js ~ line 8 ~ RecordLedger ~ accounts", accounts)
+const balObj = {
+  account: "",
+  bal: "",
+};
+
+const RecordLedger = ({ array, keys, accounts }) => {
+  // console.log("🚀 ~ file: trailRecord.js ~ line 10 ~ RecordLedger ~ array", array)
+  // console.log("🚀 ~ file: trailRecord.js ~ line 10 ~ RecordLedger ~ accounts", accounts)
+  // console.log("🚀 ~ file: trailRecord.js ~ line 10 ~ RecordLedger ~ keys", keys)
+  // console.log("🚀 ~ file: recordLedger.js ~ line 8 ~ RecordLedger ~ accounts", accounts)
   // console.log("🚀 ~ file: recordLedger.js ~ line 10 ~ RecordLedger ~ accounts", accounts,keys)
 
   // console.log(array)
@@ -14,21 +23,23 @@ const RecordLedger = ({ array, keys, accounts, bal }) => {
   const dbRef = ref(FirebaseStack());
   const [balance, setBalance] = useState(0);
   const [titles, setTitles] = useState();
-  var arrDr =[];
+  var arrDr = [];
 
-  
   const [debitDemo, setDebitDemo] = useState(0);
   // console.log("🚀 ~ file: trailRecord.js ~ line 20 ~ RecordLedger ~ debitDemo", debitDemo)
 
+  const {
+    debitBal,
+    setDebitBal,
+    creditBal,
+    setCreditBal,
+    balances,
+    setBalances,
+  } = useContext(TrailBalanceContext);
+    // console.log("🚀 ~ file: trailRecord.js ~ line 39 ~ RecordLedger ~ balances", balances)
 
 
-const {debitBal,setDebitBal,creditBal,setCreditBal} = useContext(TrailBalanceContext)
-
-
-
-
-
-
+  
 
   useEffect(() => {
     getAccountsFromFirebase();
@@ -51,7 +62,7 @@ const {debitBal,setDebitBal,creditBal,setCreditBal} = useContext(TrailBalanceCon
         console.error(error);
       });
   };
-  
+
   // const entry = [];
 
   var today = new Date();
@@ -60,102 +71,78 @@ const {debitBal,setDebitBal,creditBal,setCreditBal} = useContext(TrailBalanceCon
   }-${today.getDate()}`;
   var time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
 
-
-  var arrObj = []
-  array && array.map((obj)=>{
-    arrObj.push(obj.data)
-  })
-
+  var arrObj = [];
+  array &&
+    array.map((obj) => {
+      arrObj.push(obj.data);
+    });
 
   const getValue = (value) =>
-  typeof value === "string" ? value.toUpperCase() : value;
-  
+    typeof value === "string" ? value.toUpperCase() : value;
+
   function filterPlainArray(array, filters) {
     const filterKeys = Object.keys(filters);
     return array.filter((item) => {
-        return filterKeys.some((key) => {
-            if (!filters[key].length) return true;
+      return filterKeys.some((key) => {
+        if (!filters[key].length) return true;
         return filters[key].find(
           (filter) => getValue(filter) === getValue(item[key])
         );
       });
     });
-}
-
-
-const propertyValues = Array(accounts[keys]);
-// console.log("🚀 ~ file: recordLedger.js ~ line 101 ~ RecordLedger ~ propertyValues", propertyValues)
-
-
-const filters = {
-    title: propertyValues,
-};
-
-const filtered = filterPlainArray(arrObj, filters);
-//   console.log("🚀 ~ file: recordLedger.js ~ line 109 ~ RecordLedger ~ filtered", filtered)
-//   console.log("🚀 ~ file: recordLedger.js ~ line 108 ~ RecordLedger ~ filtered", filtered)
-//   console.log("🚀 ~ file: recordLedger.js ~ line 96 ~ RecordLedger ~ array", array)
-
-
-  useEffect(()=>{
-      getTrailBalance()
-  },[filtered])
-
-  const getTrailBalance = () => {
-      var balance = 0;
-      var Dr = 0;
-      var Cr = 0;
-      filtered && filtered.map((obj) => {
-          if (obj.type === `debit`) {
-              balance = (Number(obj.amount)+balance)
-              setBalance(balance)
-              
-          }
-          else if(obj.type === 'credit'){
-              balance = (balance-(Number(obj.amount)))
-              setBalance(balance)
-          }
-      })
-      
   }
 
+  const propertyValues = Array(accounts[keys]);
+  // console.log("🚀 ~ file: recordLedger.js ~ line 101 ~ RecordLedger ~ propertyValues", propertyValues)
 
+  const filters = {
+    title: propertyValues,
+  };
 
+  const filtered = filterPlainArray(arrObj, filters);
+  //   console.log("🚀 ~ file: recordLedger.js ~ line 109 ~ RecordLedger ~ filtered", filtered)
+  //   console.log("🚀 ~ file: recordLedger.js ~ line 108 ~ RecordLedger ~ filtered", filtered)
+  //   console.log("🚀 ~ file: recordLedger.js ~ line 96 ~ RecordLedger ~ array", array)
 
+  useEffect(() => {
+    getTrailBalance();
+  }, [filtered]);
 
+  const getTrailBalance = () => {
+    var balance = 0;
+    var Dr = 0;
+    var Cr = 0;
+    filtered &&
+      filtered.map((obj) => {
+        if (obj.type === `debit`) {
+          balance = Number(obj.amount) + balance;
+          setBalance(balance);
+        } else if (obj.type === "credit") {
+          balance = balance - Number(obj.amount);
+          setBalance(balance);
+        }
+      });
+  };
 
-
-
-
-
-
+  const loop = array.length;
 
   return (
     <>
-    
-      {
-        balance===0?
-        
-        null
-        :
-
-                <tbody>
-                    <tr>
-                      <td>date</td>
-                      <td colSpan={4} className="capitalize">{accounts[keys]}</td> 
-                      <td className="balanace">
-                        {balance>0? "$ "+balance:null}
-                      </td> 
-                      <td className="balanace">
-                        
-                        {balance<0?"$ "+Math.abs(balance):null}
-                      </td>
-                    </tr>
-                  </tbody>       
-
-
-      }
-    
+      {balance === 0 ? null : (
+        <tbody>
+          <tr>
+            <td></td>
+            <td colSpan={4} className="capitalize">
+              {accounts[keys]}
+            </td>
+            <td className="balanace">{balance > 0 ? "$ " + balance : null}</td>
+            <td className="balanace">
+              {balance < 0 ? "$ " + Math.abs(balance) : null}
+            </td>
+          </tr>
+          {/* <TrailSet data={balance} loop={loop} /> */}
+        </tbody>
+      )}
     </>
   );
 };
